7a87421bb24fd2cd537afcbe6cd67abfbc206c84
anfeng <532677481@qq.com>
2025-05-11 16:43:51 +0800

    修改了部分方法

--- src/main/java/com/example/testgit/TestController.java
+++ src/main/java/com/example/testgit/TestController.java
@@ -2,4 +2,9 @@ package com.example.testgit;
 
 public class TestController {
     public static final String HELLO_WORLD = "Hello World";
+
+    public static void main(String[] args) {
+        // 简单测试一下git提交
+        System.out.println(HELLO_WORLD);
+    }
 }
